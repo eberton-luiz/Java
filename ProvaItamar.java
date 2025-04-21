8b122067f54ed179cb0994d1fb53844cf6400742
eberton-luiz <eberton.luiz@gmail.com>
2025-04-21 19:43:05 -0300

    Add files via upload

---
+++ ProvaItamar.java
@@ -0,0 +1,55 @@
+package ebertonProject;
+
+public class ProvaItamar {
+
+	public static void main(String[] args) {
+		
+	/*	double pao = 0.50;
+		double leite = 4.20;
+		double total = 0.00;
+		
+		total = (pao * 3) + (leite * 2);
+		
+		System.out.println("O valor total da compra na padaria foi: " + total);
+		
+		float luz = 250;
+		float aluguel = 1200;
+		float cartaoCredito = 890;
+		float internet = 100;
+		float mercado = 650;
+		float salario = 3500;
+		float totalDespesa = 0;
+		float sobraMes = 0;
+		
+		totalDespesa = luz + aluguel + cartaoCredito + internet + mercado;
+		sobraMes = salario - totalDespesa;
+		
+		System.out.println("Salário: " + salario);
+		System.out.println("Despesas: " + totalDespesa);
+		System.out.println("Sobra do mês: " + sobraMes);
+		
+		*/
+		
+		int a = 7;
+		int b = 2;
+		int c = 1;
+		int d = 10;
+		int e = 1;
+		int x = 3;
+		int y = 9;
+		int z = 7;
+		
+		String resultado1 = "";
+		resultado1 = resultado1 + ((z + e)+(d + a)+(x * d));
+		resultado1 = resultado1 + ((b * d)+(b / e)+(x / a)+(d + y)+(y - d));
+		resultado1 = resultado1 + ((d + x)+(z * x)+(c / b)+(b - c));
+		resultado1 = resultado1 + ((x - e)+(a / e)+(z - b)+(c - b));
+		
+		System.out.println("Resultado: " + resultado1);
+		
+		
+		
+
+	}
+
+}
